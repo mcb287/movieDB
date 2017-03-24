f59e1e162d30a3f71c204ee87a669cff661c9ab6
mcb287 <peter.adermark@gmail.com>
2017-03-24 15:45:15 +0100

    add media and entertainment center

--- media.py
+++ media.py
@@ -1,3 +1,6 @@
-Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:24:40) [MSC v.1500 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
