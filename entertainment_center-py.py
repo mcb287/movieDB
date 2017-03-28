1affb6291e3abfa946aadaf7e78cc7edd2a7c4ad
Peter Adermark <Peter Adermark>
2017-03-28 21:19:53 +0200

    add all lessons

--- entertainment_center-py.py
+++ entertainment_center-py.py
@@ -1,4 +1,5 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy story",
                         "A story of a boy and his toys that come to life",
@@ -9,4 +10,29 @@ avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
-avatar.show_trailer()
+
+
+school_of_rock = media.Movie("School of Rock",
+                             "Storyline",
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille",
+                          "Storyline",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "Storyline",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+hunger_games = media.Movie("Hunger Games",
+                           "Storyline",
+                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "https://www.youtube.com/watch?v=mfmrPu43DF8")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+#print media.Movie.VALID_RATINGS
+print media.Movie.__module__
+#fresh_tomatoes.open_movies_page(movies)
--- inheritance.py
+++ inheritance.py
@@ -3,14 +3,24 @@ class Parent():
         print "Parent constructor called"
         self.last_name = last_name
         self.eye_color = eye_color
+    def show_info(self):
+        print "Last Name - " + self.last_name
+        print "Eye color - " + self.eye_color
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print "Child constructor called"
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
-#billy_cyrus = Parent("Cyrus", "blue")
+    def show_info(self):
+        Parent.show_info()
+        print "Last Name - " + self.last_name
+        print "Eye color - " + self.eye_color
+        print "Number of Toys - " + str(self.number_of_toys)
+billy_cyrus = Parent("Cyrus", "blue")
 miley_cyrus = Child("Cyrus", "Blue", 5)
-print miley_cyrus.last_name
-print miley_cyrus.number_of_toys
+#billy_cyrus.show_info()
+miley_cyrus.show_info()
+#print miley_cyrus.last_name
+#print miley_cyrus.number_of_toys
 #print billy_cyrus.eye_color
