83bbe4fab975c2e0f0f6f02652deb214195e42d6
mcb287 <peter.adermark@gmail.com>
2017-03-28 20:15:15 +0200

    add recent lessons

--- entertainment_center-py.py
+++ entertainment_center-py.py
@@ -4,4 +4,9 @@ toy_story = media.Movie("Toy story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-print toy_story.storyline
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+avatar.show_trailer()
